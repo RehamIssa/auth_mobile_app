977bc8177fd4c31829d15c027255f66368fe3321
RehamIssa <reham.eissa1234@gmail.com>
2025-10-01 03:04:44 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:auth_mobile_app/views/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AuthMobileApp());
+}
+
+class AuthMobileApp extends StatelessWidget {
+  const AuthMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginView(),
+    );
+  }
+}

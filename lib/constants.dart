6a4a1583ca448b8bd8bd3acffbeb5808a991b77c
RehamIssa <reham.eissa1234@gmail.com>
2025-10-01 04:01:39 +0300

    creating constants file

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Colors.white;
+const Color primaryColor = Color(0xff007BFF);
+const String defaultUserProfilePic = 'assets/images/default_profile_image.png';
